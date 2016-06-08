41b26839a1805178c4c3c2b9d9a54d6763804d2c
Kevin VanEvery and Matthew Giem <kevin@>
2016-06-08 10:16:48 -0700

    add business logic for evaluating words that start with vowels or single consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-/*This file is for your custom js.  All yours*/
+// backend logic
+var pigLatin = function(string) {
+  if (vowel(string.charAt(0))) {
+    string += "ay";
+  } else {
+    var num = 1;
+    string = string.substr(num) + string.substr(0, num) + ("ay");
+  }
+  return string;
+}
+
+var vowel = function(letter) {
+  return (/^[aeiou]$/i).test(letter);
+}
+
+// frontend logic
+$(document).ready(function(){
+  $("form").submit(function(event) {
+    var string = $("input").val();
+    string = pigLatin(string);
+    alert(string);
+    event.preventDefault();
+  })
+})
