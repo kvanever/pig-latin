adc6da266fd22ca600f21b637b82b9e6755b94ad
Kevin VanEvery and Matthew Giem <kevin@>
2016-06-08 10:43:40 -0700

    evaluate words beginning with qu syllables or consonants with qu aftet them

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,19 @@ var pigLatin = function(string) {
   debugger;
   if (vowel(string.charAt(0))) {
     string += "ay";
-  } else {
-    for (var firstVowelIndex = 0; vowel(string.charAt(firstVowelIndex)) === false; firstVowelIndex++) {
-      // return firstVowelIndex;
-    }
-    string = string.substr(firstVowelIndex) + string.substr(0, firstVowelIndex) + "ay";
   }
+  else {
+    for (var firstVowelIndex = 0; vowel(string.charAt(firstVowelIndex)) === false; firstVowelIndex++) {
+
+    }
+    if (string.charAt(firstVowelIndex-1) === "q" && string.charAt(firstVowelIndex) === "u") {
+      string = string.substr(firstVowelIndex + 1) + string.substr(0, firstVowelIndex + 1) + "ay";
+    }
+    else {
+      string = string.substr(firstVowelIndex) + string.substr(0, firstVowelIndex) + "ay";
+    }
   return string;
+  }
 }
 
 var vowel = function(letter) {
